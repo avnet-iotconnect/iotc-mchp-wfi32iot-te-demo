0c5df867f52074b4500bb236f594be8716a97739
Nikola Markovic <nikola.m@gmail.com>
2022-11-01 10:38:15 -0500

    setup-project.sh fix

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd "`dirname $0`/.."
+
 set -e
-git update-index --no-assume-unchanged firmware/src/azure_rtos_demo/sample_azure_iot_embedded_sdk/sample_device_identity.c
-git update-index --no-assume-unchanged firmware/src/azure_rtos_demo/sample_azure_iot_embedded_sdk/sample_config.h
+git update-index --assume-unchanged firmware/src/azure_rtos_demo/sample_azure_iot_embedded_sdk/sample_device_identity.c
+git update-index --assume-unchanged firmware/src/azure_rtos_demo/sample_azure_iot_embedded_sdk/sample_config.h
 echo Done
